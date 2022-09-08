b85284f19464869695f8de392b881cc98883fad8
brando allen <111488269+badong0@users.noreply.github.com>
2022-09-08 14:30:51 +0800

    Create app.java

---
+++ exam1/app.java
@@ -0,0 +1,6 @@
+public class app {
+public static void main(String[] args) {
+    student nameStudent = new student ("Donato ", "Brando Allen ", " A., ",  " July 6, 2003, ", 2022101016, " donatoba@students.nation-u.edu.ph, ", true);
+    nameStudent.sayMyName();
+    }
+}
